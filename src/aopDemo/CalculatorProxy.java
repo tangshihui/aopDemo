83c7a6eb1466dd61b94d135888f6ce024e75a4b5
tangshihui <761034176@qq.com>
2018-04-01 17:22:56 +0800

    add aop notice type note‘

--- src/aopDemo/CalculatorProxy.java
+++ src/aopDemo/CalculatorProxy.java
@@ -7,31 +7,39 @@ import java.util.Arrays;
 
 public class CalculatorProxy {
 
-	private Calculator target; 
+	private Calculator target;
 	private Class targetClass;
 
-	public CalculatorProxy(Calculator target,Class targetClass) {
+	public CalculatorProxy(Calculator target, Class targetClass) {
 		this.target = target;
-		this.targetClass=targetClass;
+		this.targetClass = targetClass;
 	}
 
 	public Calculator getCalculator() {
 		Calculator proxy = null;
-		
+
 		ClassLoader loader = this.target.getClass().getClassLoader();
 		Class[] interfaces = new Class[] { this.targetClass };
 		InvocationHandler h = new InvocationHandler() {
 			@Override
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 				String methodName = method.getName();
-				
+
 				System.out.println("this is logging...");
 				System.out.println("method:" + methodName);
-				System.out.println("args:"+Arrays.asList(args));
-				
-				
-				Object result = method.invoke(target, args);
-				
+				System.out.println("args:" + Arrays.asList(args));
+
+				// 前置通知
+				Object result = null;
+				try {
+					result = method.invoke(target, args);
+					// 返回通知
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					// 异常通知
+				}
+				// 后置通知
 				return result;
 
 			}
